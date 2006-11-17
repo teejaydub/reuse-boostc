1329cd81a1f85dcd942c56c2a423a44178307b35
teejaydub <tw@6sys.net>
2006-11-17 14:37:15 +0000

    Added some bit manipulation routines to types-tjw.h.
    git-svn-id: file:///mnt/c/tjw/dev/svn/pub/trunk/ReuseEE/PIC/BoostC@573 047a101a-1a06-0410-8d46-d6dd5a16ef5a

--- types-tjw.h
+++ types-tjw.h
@@ -5,4 +5,19 @@
 // Unsigned chars are used all the time -- cut down on typing.
 typedef unsigned char byte;
 
+// Returns a bitmask for the specified bit number.
+#define BITMASK(bitNum)  (1 << bitNum)
+
+// Sets the specified bit in the specified register based on the given expression.
+inline void setBit(char& reg, byte bitNum, byte testValue)
+{
+	if (testValue) 
+		reg |= BITMASK(bitNum); 
+	else 
+		reg &= (~BITMASK(bitNum)); 
+}
+
+// Toggles the specified bit in the specified register.
+#define toggleBit(reg, bitNum)  (reg ^= BITMASK(bitNum))
+
 #endif
