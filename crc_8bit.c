2d9bcdce793562566d7dc349298b2e81b404c744
teejaydub <tw@6sys.net>
2007-02-02 15:27:06 +0000

    Moved to the table-driven implementation of CRC, since we have the space, and speed can't hurt.
    git-svn-id: file:///mnt/c/tjw/dev/svn/pub/trunk/ReuseEE/PIC/BoostC@585 047a101a-1a06-0410-8d46-d6dd5a16ef5a

--- crc_8bit.c
+++ crc_8bit.c
@@ -19,22 +19,20 @@
 	Modified by Timothy Weber.
 */
 
+#include <system.h>
+
 #define IN_CRC_8BIT
 
 #include "crc_8bit.h"
 
 
-// Choose your favorite implementation.
-#define IMP_TABLE  1
-#define IMP_NIBBLES  2
-#define IMP_BITS  3
-
-#ifndef CRC8_IMPLEMENTATION
- #define CRC8_IMPLEMENTATION  IMP_BITS
+// Choose your favorite implementation by defining (only) one of these macros.
+#if !defined(CRC8_IMP_TABLE) && !defined(CRC8_IMP_NIBBLES) && !defined(CRC8_IMP_BITS)
+ #define CRC8_IMP_BITS
 #endif
 
 
-#if CRC8_IMPLEMENTATION == IMP_TABLE
+#ifdef CRC8_IMP_TABLE
 
 // crc array from the Maxim ApNote
 rom char* crc_array = {
@@ -82,15 +80,15 @@ unsigned char crc8(unsigned char data)
 
 #endif
 
-#if CRC8_IMPLEMENTATION == IMP_NIBBLES
+#ifdef CRC8_IMP_NIBBLES
 
 // CRC arrays for the nibble-wise routine.
-const unsigned char r1[] = {
+rom char* r1 = {
 	0x00, 0x5e, 0xbc, 0xe2, 0x61, 0x3f, 0xdd, 0x83, 
 	0xc2, 0x9c, 0x7e, 0x20, 0xa3, 0xfd, 0x1f, 0x41, 
 };
 
-const unsigned char r2[] = {
+rom char* r2 = {
 	0x00, 0x9d, 0x23, 0xbe, 0x46, 0xdb, 0x65, 0xf8,
 	0x8c, 0x11, 0xaf, 0x32, 0xca, 0x57, 0xe9, 0x74
 };
@@ -104,7 +102,7 @@ unsigned char crc8(unsigned char data)
 }
 #endif
 
-#if CRC8_IMPLEMENTATION == IMP_BITS
+#ifdef CRC8_IMP_BITS
 unsigned char crc8(unsigned char data)
 {
 	unsigned char i = (data ^ crc);
