506cb0960c6ac85d6ca493735cfd312f56989523
teejaydub <tw@6sys.net>
2007-04-16 21:35:20 +0000

    Added chipinit.h, to encapsulate common initialization stuff that I often get wrong.
    git-svn-id: file:///mnt/c/tjw/dev/svn/pub/trunk/ReuseEE/PIC/BoostC@590 047a101a-1a06-0410-8d46-d6dd5a16ef5a

---
+++ chipinit.h
@@ -0,0 +1,28 @@
+/* chipinit.h
+
+	Standard code to initialize PICs.
+	
+	Packaged in inline functions, because it plays easier with the compilers
+	(can't specify source files in other directories!)
+	and because this code is typically only used once, so inline doesn't cost anything.
+	
+	As I use new chips, I'll add chip-specific sections.
+*/
+
+#include <system.h>
+
+// Disables all optional peripherals, including:
+//  Comparators
+//  A/D converters
+inline void DisablePeripherals(void)
+{
+	#if defined(_PIC12F629) || defined(_PIC12F675)
+		// Disable comparators.
+		cmcon = 7;
+	#endif
+	
+	#if defined(_PIC12F675)
+		// Disable A/D.
+		ansel = 0;
+	#endif
+}
