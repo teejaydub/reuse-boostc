8af3f0184fc1246e62031914f675e7e4b6ef7f14
teejaydub <tw@6sys.net>
2007-05-21 13:57:16 +0000

    Added PlaySound and PlayClick to BlockingSound.
    git-svn-id: file:///mnt/c/tjw/dev/svn/pub/trunk/ReuseEE/PIC/BoostC@597 047a101a-1a06-0410-8d46-d6dd5a16ef5a

--- BlockingSound.c
+++ BlockingSound.c
@@ -1,2 +1,59 @@
 // BlockingSound.c
 
+#include <system.h>
+
+#include "BlockingSound.h"
+#include "BlockingSoundConsts.h"
+#include "types-tjw.h"
+
+void PlaySound(unsigned short periodUs, unsigned short durationMs)
+{
+	// Prepare the port.
+	SOUND_TRIS.SOUND_PIN = 0;
+	
+	// Think in terms of half-period, not periods.
+	periodUs >>= 1;
+	durationMs <<= 1;
+	
+	// Cut the period into two byte values we can pass to the standard delay functions.
+	byte periodm = periodUs / 1000;
+	byte period10u = (periodUs % 1000) / 10;
+	
+	intcon.GIE = 0;
+	
+	unsigned short usPlayed = 0;
+	unsigned short msPlayed = 0;
+	while (msPlayed < durationMs) {
+		// Play for one period.
+		
+		// Toggle the sound pin.
+		SOUND_LATCH ^= SOUND_MASK;
+		
+		// Delay.
+		if (periodm)
+			delay_ms(periodm);
+		if (period10u)
+			delay_10us(period10u);
+		
+		// Accumulate the total time.
+		usPlayed += periodUs;
+		
+		if (usPlayed > 1000) {
+			usPlayed -= 1000;
+			++msPlayed;
+		}
+	}		
+	
+	intcon.GIE = 1;
+}
+
+void PlayClick(void)
+{
+	// Prepare the port.
+	SOUND_TRIS.SOUND_PIN = 0;
+	
+	// Just toggle the pin state once.
+	//SOUND_LATCH ^= SOUND_MASK;
+	
+	PlaySound(1000, 2);
+}
--- BlockingSound.h
+++ BlockingSound.h
@@ -4,5 +4,19 @@
 	
 	Blocks on each call, and turns off interrupts,
 	so only useful when there's no multitasking going on.
+	
+	Requires BlockingSoundConsts.h defined.
 */
 
+#ifndef __BLOCKING_SOUND_H
+#define __BLOCKING_SOUND_H
+
+// Plays a square wave with the given period (in microseconds) 
+// for the given duration (in milliseconds).
+// Duration can range up to 32767 ms.
+void PlaySound(unsigned short periodUs, unsigned short durationMs);
+
+// Makes a short click.
+void PlayClick(void);
+
+#endif
