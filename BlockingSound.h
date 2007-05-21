abf482b71d592cf711ed193ca0f2054b127b6477
teejaydub <tw@6sys.net>
2007-05-21 12:52:29 +0000

    Added BlockingSound stub to Reuse.
    git-svn-id: file:///mnt/c/tjw/dev/svn/pub/trunk/ReuseEE/PIC/BoostC@596 047a101a-1a06-0410-8d46-d6dd5a16ef5a

---
+++ BlockingSound.h
@@ -0,0 +1,8 @@
+/* BlockingSound.h
+
+	Sound generation through an arbitrary port pin, using no timers.
+	
+	Blocks on each call, and turns off interrupts,
+	so only useful when there's no multitasking going on.
+*/
+
