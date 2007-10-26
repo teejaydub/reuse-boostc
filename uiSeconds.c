c1317250fb8d1b6cd4a02333b7bc3476c8f5e866
teejaydub <tw@6sys.net>
2007-10-26 16:58:45 +0000

    Simplified UpdateUISeconds.
    git-svn-id: file:///mnt/c/tjw/dev/svn/pub/trunk/ReuseEE/PIC/BoostC@1200 047a101a-1a06-0410-8d46-d6dd5a16ef5a

--- uiSeconds.c
+++ uiSeconds.c
@@ -10,43 +10,22 @@
 
 
 
-// Number of ticks since we last updated.  Don't modify outside this module.
-unsigned char secondTicks = 0;
-
-// Extra ticks that we haven't yet factored in.
-unsigned char spareSecondTicks = 0;
+// Tick value when we last updated.
+static byte secondTicks = 0;
 
 
 void ClearUiSeconds(void)
 {
 	seconds = 0;
 	secondTicks = ticks;
-	spareSecondTicks = 0;
 }
 
 void UpdateUiSeconds(void)
 {
-	#if 0
-	unsigned startTicks = ticks;
-	unsigned char elapsedSecs = startTicks - secondTicks;
-	
-	if (elapsedSecs) {
-		elapsedSecs += spareSecondTicks;
-		spareSecondTicks = elapsedSecs & (TICKS_PER_SEC - 1);
-		elapsedSecs = elapsedSecs >> LOG2_TICKS_PER_SEC;
-		
-		seconds += elapsedSecs;
-	
-		secondTicks = startTicks;
-	}
-	#else
-	
-	byte startTicks = ticks;
-	byte elapsedTicks = startTicks - secondTicks;
+	byte elapsedTicks = ticks - secondTicks;
 	while (elapsedTicks >= TICKS_PER_SEC) {
 		++seconds;
 		elapsedTicks -= TICKS_PER_SEC;
 		secondTicks += TICKS_PER_SEC;
 	}
-	#endif
 }
